5b5442760f9e3a85db9a7bfba5789cf1be89daaa
Amr Odhymi <amrodhymi@gmail.com>
2025-07-05 18:50:38 +0300

    edit file

---
+++ StudentAttendanceSys/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StudentAttendanceSys.Forms.LoginForm;
+
+namespace StudentAttendanceSys
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
