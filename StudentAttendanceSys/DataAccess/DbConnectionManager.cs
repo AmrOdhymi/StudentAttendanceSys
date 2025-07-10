3cfa60217536b850988ba6a1fcabce6b9df91011
Amr Odhymi <amrodhymi@gmail.com>
2025-07-10 18:36:27 +0300

    add class DbConnectionManager

---
+++ StudentAttendanceSys/DataAccess/DbConnectionManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentAttendanceSys.DataAccess
+{
+    public class DbConnectionManager
+    {
+        SqlConnection sqlConnection;
+
+
+        //database Connection
+        public DbConnectionManager()
+        {
+            //اسم السرفر ثم اسم قاعدة البيانات ثم طريقة الدخول الى السرفر
+            sqlConnection = new SqlConnection(@"Servre=.\SQLEXPRESS; Database=StudentAttendanceDB;Integrated Security=true");
+        }
+
+        // open database Connection
+        public void Open()
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+                sqlConnection.Open();
+        }
+
+        // close database Connection
+        public void Close()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+                sqlConnection.Close();
+        }
+    }
+}
