b5549bae7ddb1540fb59fee40fb2199e555b6871
Amr Odhymi <amrodhymi@gmail.com>
2025-07-11 20:18:20 +0300

    student user controle

--- StudentAttendanceSys/Forms/AdminForms/StudentManagement/UCStudentManagement/UCStudentManagement.Designer.cs
+++ StudentAttendanceSys/Forms/AdminForms/StudentManagement/UCStudentManagement/UCStudentManagement.Designer.cs
@@ -28,25 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBoxViewDepartments = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(0, 93);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.RowHeadersWidth = 51;
+            this.dataGridViewStudents.RowTemplate.Height = 24;
+            this.dataGridViewStudents.Size = new System.Drawing.Size(792, 357);
+            this.dataGridViewStudents.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(255, 155);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "UCStudentManagement";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "StudentManagement";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(391, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(361, 64);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "بحث بالاسم او الرقم التعريفي";
+            // 
+            // listBoxViewDepartments
+            // 
+            this.listBoxViewDepartments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxViewDepartments.FormattingEnabled = true;
+            this.listBoxViewDepartments.ItemHeight = 20;
+            this.listBoxViewDepartments.Location = new System.Drawing.Point(3, 23);
+            this.listBoxViewDepartments.Name = "listBoxViewDepartments";
+            this.listBoxViewDepartments.Size = new System.Drawing.Size(163, 38);
+            this.listBoxViewDepartments.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxViewDepartments);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(216, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(169, 64);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "فلتره حسب القسم";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dateTimePicker1);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(44, 26);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox3.Size = new System.Drawing.Size(169, 64);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "فلتره حسب التاريخ";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 23);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(163, 27);
+            this.dateTimePicker1.TabIndex = 0;
             // 
             // UCStudentManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewStudents);
             this.Name = "UCStudentManagement";
-            this.Size = new System.Drawing.Size(592, 388);
+            this.Size = new System.Drawing.Size(792, 453);
+            this.Load += new System.EventHandler(this.UCStudentManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +151,14 @@
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBoxViewDepartments;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- StudentAttendanceSys/Forms/AdminForms/StudentManagement/UCStudentManagement/UCStudentManagement.cs
+++ StudentAttendanceSys/Forms/AdminForms/StudentManagement/UCStudentManagement/UCStudentManagement.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient; //مؤقت
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StudentAttendanceSys.DataAccess;
 
 namespace StudentAttendanceSys.Forms.AdminForms.StudentManagement.UCStudentManagement
 {
     public partial class UCStudentManagement : UserControl
     {
+        DbConnectionManager dbManager = new DbConnectionManager();
         public UCStudentManagement()
         {
             InitializeComponent();
+            dateTimePicker1.CustomFormat = "yyyy";
         }
+
+        private void UCStudentManagement_Load(object sender, EventArgs e)
+        {
+        }
+
     }
 }
